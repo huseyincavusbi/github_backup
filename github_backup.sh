aadc439570f8e360aab97c38fbab393634c21d11
Hüseyin Çavuş <huseyincavus@proton.me>
2025-08-17 18:19:40 +0300

    Update github_backup.sh
    Added feature to save backup date information

--- github_backup.sh
+++ github_backup.sh
@@ -7,7 +7,7 @@
 GITHUB_USER="YOUR_USERNAME"
 
 # Directory to save the backups in.
-BACKUP_DIR="/path/to/your/backups"
+BACKUP_DIR="/workspaces/codespace/backups/backup_$(date +%m_%d_%y)"
 
 # The maximum number of repositories to back up at the same time.
 # Start with a low number (e.g., 4) and increase it based on your
